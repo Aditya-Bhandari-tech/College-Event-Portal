9de5237ff041820ee9a977b270f71cef80711008
Aditya-Bhandari-tech <adityabhandari0153@gmail.com>
2025-11-25 19:52:08 +0530

    Apply .gitignore and remove .env from tracking

---
+++ backend/readme.md
@@ -0,0 +1,21 @@
+# College Event Portal - Backend
+
+## Scripts
+
+### npm install
+Installs all project dependencies from package.json
+
+### npm start
+Runs the server in production mode on port 5000
+
+### npm run dev
+Runs the server in development mode with auto-restart using nodemon
+
+
+npm init -y
+ npm install express mongoose dotenv cors
+  npm install --save-dev nodemon
+        "scripts": {
+  "start": "node server.js",
+  "dev": "nodemon server.js"
+} change script like this!!
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-ECHO is on.
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend working!");
+});
+
+const PORT = process.env.PORT || 5000;
+console.log("URI =", process.env.MONGO_URI);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
